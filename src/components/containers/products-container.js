a4b330f15546a0b2cd3a92bd693315a0ff4edc09
kreuzfeur <kreuzfeur@gmail.com>
2019-12-15 08:54:33 +0300

    added function onUpdatePage

--- src/components/containers/products-container.js
+++ src/components/containers/products-container.js
@@ -20,45 +20,37 @@ import { withRouter } from 'react-router-dom'
 
 class ProductsContainer extends Component {
 	componentDidMount = () => {
-		const { updateItemsError, updateItemsPending, updateItemsSuccess, updatePaginationSuccess, page } = this.props;
-		document.title = `Страница ${page}`
-		updateItemsPending();
-		this.props.apiService.getProducts(page - 1)
-			.then(response => {
-				updateItemsSuccess(response.data.items)
-				updatePaginationSuccess(response.data.pagination)
-			})
-			.catch(err => {
-				updateItemsError(err)
-			})
+		this.onUpdatePage(this.props.page - 1)
 	}
 	componentDidUpdate = (prevState) => {
 		if (prevState.pagination.currentPage !== this.props.pagination.currentPage) {
-			const { updateItemsError, updateItemsPending, updateItemsSuccess, updatePaginationSuccess, pagination, page } = this.props;
-			document.title = `Страница ${page}`
-			updateItemsPending();
-			this.props.apiService.getProducts(pagination.currentPage)
-				.then(response => {
-					updateItemsSuccess(response.data.items)
-					updatePaginationSuccess(response.data.pagination)
-				})
-				.catch(err => {
-					updateItemsError(err)
-				})
+			this.onUpdatePage(this.props.pagination.currentPage)
 		}
 	}
+	onUpdatePage = (changedPage) => {
+		const { updateItemsError, updateItemsPending, updateItemsSuccess, updatePaginationSuccess, pagination, page } = this.props;
+		document.title = `Страница ${page}`;
+		updateItemsPending();
+		this.props.apiService.getProducts(changedPage)
+			.then(response => {
+				updateItemsSuccess(response.data.items);
+				updatePaginationSuccess(response.data.pagination);
+			})
+			.catch(err => {
+				updateItemsError(err);
+			})
+	}
 	render() {
-		const { loading, items, pagination, updateCurrentPage, history, match} = this.props;
+		const { loading, items, pagination, updateCurrentPage, history, match } = this.props;
 		const loader = (loading) ? <Preloader /> : null;
-		// const displayError = (error) ? error : null;
 		const products = items.map(({ id, location, saleOffer, specification, landDetails, images }) => {
-			let price = 'нет цены'
+			let price = 'нет цены';
 			if (saleOffer) {
-				price = saleOffer.multiCurrencyPrice.usd
+				price = saleOffer.multiCurrencyPrice.usd;
 			}
-			let imgUrl = false
+			let imgUrl = false;
 			if (images[0]) {
-				imgUrl = `http://images.jqestate.ru/${images[0].id}-thumbnail-512`
+				imgUrl = `http://images.jqestate.ru/${images[0].id}-thumbnail-512`;
 			}
 			return <Product
 				key={id}
